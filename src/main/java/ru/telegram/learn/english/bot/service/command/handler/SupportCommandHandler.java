153c24942dcb87f4809c7e63e9a9d2689bc1dc74
kalyashov <kalyashov@yandex.ru>
2021-09-11 10:55:08 +0300

    Добавил обработчик для команды /support

---
+++ src/main/java/ru/telegram/learn/english/bot/service/command/handler/SupportCommandHandler.java
@@ -0,0 +1,30 @@
+package ru.telegram.learn.english.bot.service.command.handler;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.telegram.learn.english.bot.business.action.CommandActions;
+import ru.telegram.learn.english.bot.business.action.SendMessageAction;
+import ru.telegram.learn.english.bot.config.BotConfig;
+
+import java.util.Collections;
+
+@Component
+public class SupportCommandHandler implements CommandHandler<SendMessage> {
+
+    private final BotConfig botConfig;
+
+    public SupportCommandHandler(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public CommandActions handle(Update update) {
+        String chatId = update.getMessage().getChatId().toString();
+
+        SendMessage sm = new SendMessage(chatId, botConfig.getMessages().getSupport());
+        SendMessageAction sma = new SendMessageAction(sm);
+
+        return new CommandActions(Collections.singletonList(sma));
+    }
+}
